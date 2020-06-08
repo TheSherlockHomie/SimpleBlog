c925b7789ae772bfc95192046266869233793d9a
TheSherlockHomie <coolman.kushagra@gmail.com>
2020-06-08 15:10:17 +0530

    Add ability to add posts

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -13,7 +13,14 @@ router.get("/", async (req, res) => {
 });
 
 //Add posts
-
+router.post("/", async (req, res) => {
+	const posts = await loadPostsCollection();
+	await posts.insertOne({
+		text: req.body.text,
+		createdAt: new Date(),
+	});
+	res.status(201).send();
+});
 //Delete posts
 
 async function loadPostsCollection() {
