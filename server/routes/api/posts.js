59d1be0bb3ce1da68bcdd5ed1792a790557a729c
TheSherlockHomie <coolman.kushagra@gmail.com>
2020-06-08 13:17:45 +0530

    Set up router skeleton

---
+++ server/routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+//Get posts
+
+//Add posts
+
+//Delete posts
+
+module.exports = router;
