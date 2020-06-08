13ef32a48cc18fd57e3d0d92b2dba91e7c513cdb
TheSherlockHomie <coolman.kushagra@gmail.com>
2020-06-08 14:53:09 +0530

    Add ability to get of posts from mongodb atlas

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const mongodb = require("mongodb");
+const apiKeys = require("./../../../apiKeys");
+
+const mongoLogin = apiKeys.mongoDbLogin();
 
 const router = express.Router();
 
 //Get posts
+router.get("/", async (req, res) => {
+	const posts = await loadPostsCollection();
+	res.send(await posts.find({}).toArray());
+});
 
 //Add posts
 
 //Delete posts
+
+async function loadPostsCollection() {
+	const client = await mongodb.MongoClient.connect(
+		`mongodb+srv://${mongoLogin.username}:${mongoLogin.password}@simpleblog-fgubp.mongodb.net/SimpleBlog?retryWrites=true&w=majority`,
+		{
+			useNewUrlParser: true,
+		}
+	);
+	return client.db(`SimpleBlog`).collection("posts");
+}
 
 module.exports = router;
