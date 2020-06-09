74193a93b4ae0dc659f7a057e4b805486ca6e639
TheSherlockHomie <coolman.kushagra@gmail.com>
2020-06-09 14:03:45 +0530

    Make function to get posts in post service

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -4,9 +4,25 @@ const url = "http://localhost:5000/api/posts";
 
 class PostService {
     //Get Posts
+    static getPosts() {
+        return new Promise(async (resolve, reject) =>{
+            try {
+                const res = await axios.get(url);
+                const data = res.data;
 
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                );
+            } catch(err) {
+                reject(err);
+            }
+        })
+    }
     //Create Post
 
     //Delete Post
-    
+
 }
