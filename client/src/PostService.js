5b4881932f0d7611475c1db0020160d9ba0b1ffd
TheSherlockHomie <coolman.kushagra@gmail.com>
2020-06-09 15:03:56 +0530

    Fix bugs with eslint and with import axios

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -1,38 +1,40 @@
-import axios from axios;
+/* eslint-disable */
+import axios from "axios";
 
 const url = "http://localhost:5000/api/posts";
 
 class PostService {
-    //Get Posts
-    static getPosts() {
-        return new Promise(async (resolve, reject) =>{
-            try {
-                const res = await axios.get(url);
-                const data = res.data;
+	//Get Posts
+	static getPosts() {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const res = await axios.get(url);
+				const data = res.data;
 
-                resolve(
-                    data.map(post => ({
-                        ...post,
-                        createdAt: new Date(post.createdAt)
-                    }))
-                );
-            } catch(err) {
-                reject(err);
-            }
-        })
-    }
+				resolve(
+					data.map((post) => ({
+						...post,
+						createdAt: new Date(post.createdAt),
+					}))
+				);
+			} catch (err) {
+				reject(err);
+			}
+		});
+	}
 
-    //Create Post
-    static insertPost(text) {
-        return axios.post(url, {
-            text: text
-        });
-    }
+	//Create Post
+	static insertPost(text) {
+		return axios.post(url, {
+			text: text,
+		});
+	}
 
-    //Delete Post
-    static deletePost(id) {
-        return axios.delete(`${url}/${id}`);
-    }
+	//Delete Post
+	static deletePost(id) {
+		return axios.delete(`${url}/${id}`);
+	}
 }
 
 export default PostService;
+/* eslint-enable */
